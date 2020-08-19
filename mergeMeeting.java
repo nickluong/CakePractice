aca36afa8a52339b5ebb07829d8f7fe6dbad725e
Nick Luong <nckluong@gmail.com>
2020-08-19 16:58:24 -0400

    finished merge meeting problem

---
+++ mergeMeeting.java
@@ -0,0 +1,71 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Meeting {
+
+    private int startTime;
+    private int endTime;
+
+    public Meeting(int startTime, int endTime) {
+        // number of 30 min blocks past 9:00 am
+        this.startTime = startTime;
+        this.endTime   = endTime;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+
+    public int getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(int endTime) {
+        this.endTime = endTime;
+    }
+}
+
+public static List<Meeting> mergeRanges(List<Meeting> meetings) {
+
+    // make a copy so we don't destroy the input
+    List<Meeting> sortedMeetings = new ArrayList<>();
+    for (Meeting meeting: meetings) {
+        Meeting meetingCopy = new Meeting(meeting.getStartTime(), meeting.getEndTime());
+        sortedMeetings.add(meetingCopy);
+    }
+
+    // sort by start time
+    Collections.sort(sortedMeetings, new Comparator<Meeting>() {
+        @Override
+        public int compare(Meeting m1, Meeting m2) {
+            return m1.getStartTime() - m2.getStartTime();
+        }
+    });
+
+    // initialize mergedMeetings with the earliest meeting
+    List<Meeting> mergedMeetings = new ArrayList<>();
+    mergedMeetings.add(sortedMeetings.get(0));
+
+    for (Meeting currentMeeting : sortedMeetings) {
+
+        Meeting lastMergedMeeting = mergedMeetings.get(mergedMeetings.size() - 1);
+
+        // if the current meeting overlaps with the last merged meeting, use the
+        // later end time of the two
+        if (currentMeeting.getStartTime() <= lastMergedMeeting.getEndTime()) {
+            lastMergedMeeting.setEndTime(Math.max(lastMergedMeeting.getEndTime(), currentMeeting.getEndTime()));
+
+        // add the current meeting since it doesn't overlap
+        } else {
+            mergedMeetings.add(currentMeeting);
+        }
+    }
+
+    return mergedMeetings;
+}
