26aaa7aae3cbd948eac01f68b04dbc344bdabc37
Nick Luong <nckluong@gmail.com>
2020-08-20 11:52:32 -0400

    updated mergeSorted

--- mergeSortedArrays.java
+++ mergeSortedArrays.java
@@ -1,5 +1,6 @@
 public static int[] mergeArrays(int[] myArray, int[] alicesArray) {
 
+    // set up our mergedArray
     int[] mergedArray = new int[myArray.length + alicesArray.length];
 
     int currentIndexAlices = 0;
@@ -7,17 +8,24 @@ public static int[] mergeArrays(int[] myArray, int[] alicesArray) {
     int currentIndexMerged = 0;
 
     while (currentIndexMerged < mergedArray.length) {
-        int firstUnmergedAlices = alicesArray[currentIndexAlices];
-        int firstUnmergedMine = myArray[currentIndexMine];
+
+        boolean isMyArrayExhausted = currentIndexMine >= myArray.length;
+        boolean isAlicesArrayExhausted = currentIndexAlices >= alicesArray.length;
 
         // case: next comes from my array
-        if (firstUnmergedMine < firstUnmergedAlices) {
-            mergedArray[currentIndexMerged] = firstUnmergedMine;
+        // my array must not be exhausted, and EITHER:
+        // 1) Alice's array IS exhausted, or
+        // 2) the current element in my array is less
+        //    than the current element in Alice's array
+        if (!isMyArrayExhausted && (isAlicesArrayExhausted
+                || (myArray[currentIndexMine] < alicesArray[currentIndexAlices]))) {
+
+            mergedArray[currentIndexMerged] = myArray[currentIndexMine];
             currentIndexMine++;
 
         // case: next comes from Alice's array
         } else {
-            mergedArray[currentIndexMerged] = firstUnmergedAlices;
+            mergedArray[currentIndexMerged] = alicesArray[currentIndexAlices];
             currentIndexAlices++;
         }
 
